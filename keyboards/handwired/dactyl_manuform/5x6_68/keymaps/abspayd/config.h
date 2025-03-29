5963d3e18716d919deffe1fe424c06595cc13e0c
Adam Spayd <adam.spayd@gmail.com>
2025-03-29 16:47:42 -0400

    feat: add custom Dactyl Manuform 68 config

---
+++ keyboards/handwired/dactyl_manuform/5x6_68/keymaps/abspayd/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Kostas Pagratis (@kpagratis)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SPLIT_USB_DETECT
+#define TAPPING_TERM 250
+
+#define EE_HANDS
+#define MATRIX_COL_PINS_LEFT {B5, B4, E6, D7, C6, D4}
+#define MATRIX_COL_PINS_RIGHT {D4, C6, D7, E6, B4, B5}
